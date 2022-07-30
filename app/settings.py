8560676a32aaf75294cbfb6cf2e57e1c29ca918e
unknown <sanyam.sj9797@gmail.com>
2022-07-30 19:32:23 +0530

    update

--- app/settings.py
+++ app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3i5f+#luq39%@%#rgz#tjg2!9sxhwg@z2v*2ajejl&!$y$_)+5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
