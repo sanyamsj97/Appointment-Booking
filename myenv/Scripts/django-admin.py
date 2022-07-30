dceafabcf38eeedec34ce18d611d12748cb58fde
unknown <sanyam.sj9797@gmail.com>
2022-07-30 18:48:02 +0530

    update

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\sanyam jain\downloads\django-appointment-and-booking-system-master\django-appointment-and-booking-system-master\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
